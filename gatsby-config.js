e5494523322584676fc8753d1f1058a894202a4c
Keisuke Taniguchi <rememberthatk@icloud.com>
2020-09-25 18:45:55 +0900

    [master] add gatsby-plugin-eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+      },
+    },
+  ],
 }
