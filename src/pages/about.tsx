401c8163cd0a34af5035085f2a42fc0e563d15c3
Keisuke Taniguchi <rememberthatk@icloud.com>
2020-09-25 20:27:34 +0900

    [master] add component and page

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Header from 'src/components/header'
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header />
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
